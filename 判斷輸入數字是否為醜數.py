15524dacf8eb64329aecdfbb1494f7ea482aa1a3
Hibiyayuri <69136254+Hibiyayuri@users.noreply.github.com>
2020-09-05 20:05:15 +0800

    Add files via upload

---
+++ 判斷輸入數字是否為醜數.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        n=eval(input())
+        while(n%2==0):
+            n=n/2
+        while(n%3==0):
+            n=n/3
+        while(n%5==0):
+            n=n/5
+        if(n==1):
+            print('true')
+        else:
+            print('false')
+    except(EOFError):
+        break
